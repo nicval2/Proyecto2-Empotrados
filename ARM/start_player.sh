ead7d26860c1a4033bd6ae93897da8f58c29d82a
Kun Kin Zheng Liang <106352914+kunZhen@users.noreply.github.com>
2025-12-02 17:49:17 -0600

    Revert "HPS Angstrom"

--- ARM/start_player.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# start_player.sh
-cd /home/root/proyecto_audio
-WAV_FILES=$(ls -1 *.wav 2>/dev/null | tr '\n' ' ')
-if [ -z "$WAV_FILES" ]; then
-    echo "Error: No hay archivos .wav"
-    exit 1
-fi
-exec ./wav_player $WAV_FILES
